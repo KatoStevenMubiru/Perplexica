3bebd53ec0973da1bb92aaee809ab90026691467
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-06-28 19:04:56 +0300

    Delete index.ts

--- src/config/index.ts
+++
@@ -1,7 +0,0 @@
-export const getUnifyApiKey = (): string => {
-    return process.env.UNIFY_API_KEY || '';
-  };
-  
-  export const getUnifyApiEndpoint = (): string => {
-    return process.env.UNIFY_API_ENDPOINT || 'https://api.unify.ai/v0/chat/completions';
-  };
